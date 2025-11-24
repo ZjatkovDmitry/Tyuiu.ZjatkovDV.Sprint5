332cf3f41042313ce2878c32d342280af25f8998
Dmitry <Tyuiu.ZjatkovDV@outlook.com>
2025-11-25 03:21:32 +0500

    Task6

--- Tyuiu.ZjatkovDV.Sprint5.Task6.V21.Lib/DataService.cs
+++ Tyuiu.ZjatkovDV.Sprint5.Task6.V21.Lib/DataService.cs
@@ -1,7 +1,15 @@
-﻿namespace Tyuiu.ZjatkovDV.Sprint5.Task6.V21.Lib
-{
-    public class DataService
-    {
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using tyuiu.cources.programming.interfaces.Sprint5;
+using static System.Net.Mime.MediaTypeNames;
 
+namespace Tyuiu.ZjatkovDV.Sprint5.Task6.V21.Lib
+{
+    public class DataService : ISprint5Task6V21
+    {
+        public int LoadFromDataFile(string path)
+        {
+            return Regex.Matches(File.ReadAllText(path).ToLower(), "тт").Count;
+        }
     }
 }
--- Tyuiu.ZjatkovDV.Sprint5.Task6.V21/Program.cs
+++ Tyuiu.ZjatkovDV.Sprint5.Task6.V21/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using Tyuiu.ZjatkovDV.Sprint5.Task6.V21.Lib;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.Title = "Спринт #5 | Выполнил: Зятьков Д. В. | ПИНб-25-1";
+Console.WriteLine(new string('*', 75));
+Console.WriteLine("* Спринт #5                                                               *");
+Console.WriteLine("* Тема: Обработка текстовых файлов                                        *");
+Console.WriteLine("* Задание #6                                                              *");
+Console.WriteLine("* Вариант #21                                                             *");
+Console.WriteLine("* Выполнил: Зятьков Дмитрий Владимирович | ПИНб-25-1                      *");
+Console.WriteLine(new string('*', 75));
+Console.WriteLine("* УСЛОВИЕ: Дан файл С:\\DataSprint5\\InPutDataFileTask6V21.txt, в котором   *");
+Console.WriteLine("* есть набор символьных данных. Найти количество удвоенных букв \"тт\" в    *");
+Console.WriteLine("* заданной строке.                                                        *");
+Console.WriteLine(new string('*', 75));
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine(new string('*', 75));
+
+var result = new DataService();
+
+string x = "C:\\DataSprint5\\InPutDataFileTask6V21.txt";
+
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine(result.LoadFromDataFile(x).ToString());
+
+Console.WriteLine(new string('*', 75));
+Console.ReadKey();
